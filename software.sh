6dbbb39d2382429226af43d0a778da88661918b3
Fool543 <120022950+Fool543@users.noreply.github.com>
2023-03-15 18:12:42 +0530

    Update software.sh

--- software.sh
+++ software.sh
@@ -1,1 +1,1 @@
-hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist rofi-wayland  waybar-hyprland alacritty fish pcmanfm p7zip xarchiver arc-gtk-theme network-manager-applet nvim git 
+hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist rofi-wayland  waybar-hyprland 
