f4bde13151d096586220fe25620d4ff7353219ec
Fool543 <120022950+Fool543@users.noreply.github.com>
2023-03-15 18:13:14 +0530

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,1 +1,1 @@
-noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extras ttf-firacode-nerd ttf-font-awesome polkit-gnome gnome-keyring xfce-settings xdg-desktop-portal xdg-desktop-portal-gtk viewnior dunst
+noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extras ttf-firacode-nerd ttf-font-awesome polkit-gnome gnome-keyring xfce-settings xdg-desktop-portal xdg-desktop-portal-gtk viewnior dunst alacritty fish pcmanfm p7zip xarchiver arc-gtk-theme network-manager-applet nvim git 
