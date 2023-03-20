17dc2614b91d03a79b03a9df39435a374b66b094
Fool543 <aritrad589@outlook.com>
2023-03-20 12:47:52 +0530

    Update packages

--- aur.sh
+++ aur.sh
@@ -1,1 +1,2 @@
- yay -S hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist rofi-lbonn-wayland-git  waybar-hyprland-git wlogout ferdium swaync
+ yay -S hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist rofi-lbonn-wayland-git  waybar-hyprland-git \
+ wlogout ferdium cave brave-bin sddm-git swaync nwg-look-bin --needed \
--- pacman.sh
+++ pacman.sh
@@ -1,1 +1,3 @@
-noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extras ttf-firacode-nerd ttf-font-awesome polkit-gnome gnome-keyring xfce4-settings xdg-desktop-portal viewnior alacritty fish pcmanfm p7zip xarchiver arc-gtk-theme network-manager-applet nvim git sddm 
+sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra ttf-firacode-nerd ttf-font-awesome polkit-gnome \
+gnome-keyring xfce4-settings xdg-desktop-portal viewnior alacritty fish pcmanfm p7zip xarchiver arc-gtk-theme \
+network-manager-applet neovim xf86-video-openchrome intel-media-driver --needed
