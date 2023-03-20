e53e68c38b0c7f850a0c100f9290e1d083be61b5
Fool543 <aritrad589@outlook.com>
2023-03-20 12:38:26 +0530

    Add swaync

--- aur.sh
+++ aur.sh
@@ -1,1 +1,1 @@
-hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist rofi-wayland  waybar-hyprland wlogout
+ yay -S hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist rofi-lbonn-wayland-git  waybar-hyprland-git wlogout ferdium swaync
