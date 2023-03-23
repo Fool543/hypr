3acec7f3d150d8e0ea1b56c6b407c30aca8f0849
Fool543 <120022950+Fool543@users.noreply.github.com>
2023-03-24 00:46:52 +0530

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,3 +1,3 @@
- yay -S hyprland-bin hyprpaper-git gtklock hyprshot wl-copy cliphist \
+ yay -S hyprland-bin hyprpaper-git gtklock hyprshot wl-clipboard cliphist \
  rofi-lbonn-wayland-git waybar-hyprland-git \
  wlogout ferdium cava brave-bin sddm-git swaync nwg-look-bin --needed \
