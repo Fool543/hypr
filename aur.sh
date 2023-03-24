9f8bb4ca7948c69fa8207c139f87d0afa55f4183
Fool543 <120022950+Fool543@users.noreply.github.com>
2023-03-24 17:18:19 +0530

    Update aur.sh

--- aur.sh
+++ aur.sh
@@ -1,3 +1,4 @@
  yay -S hyprland hyprpaper-git gtklock hyprshot wl-clipboard cliphist \
  rofi-lbonn-wayland-git waybar-hyprland-git \
- wlogout ferdium-bin cava brave-bin sddm-git swaync nwg-look-bin -y --needed \
+ wlogout ferdium-bin cava brave-bin sddm-git swaync nwg-look-bin \
+ linux-xanmod-tt linux-xanmod-tt-headers -y --needed \
